8cd7d3cced29b489fd211cdbbbb7ed2c860bc6fc
Reignfolly <153690940+Reignfolly@users.noreply.github.com>
2026-02-10 17:06:54 -0500

    Added Main Menu

---
+++ Neon Excimers/Assets/UI/MainMenuController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+public class MainMenuController : MonoBehaviour
+{
+    public VisualElement ui;
+
+    public Button playButton;
+    public Button optionsButton;
+    public Button quitButton;
+    
+    private void Awake()
+    {
+        ui = GetComponent<UIDocument>().rootVisualElement;
+
+        playButton = ui.Q<Button>("PlayButton");
+        optionsButton = ui.Q<Button>("OptionsButton");
+        quitButton = ui.Q<Button>("QuitButton");
+
+        playButton.clicked += OnPlayButtonClicked;
+        optionsButton.clicked += OnOptionsButtonClicked;
+        quitButton.clicked += OnQuitButtonClicked;
+    }
+    private void OnPlayButtonClicked()
+    {
+        gameObject.SetActive(false);
+    }
+    private void OnOptionsButtonClicked()
+    {
+        // Open the options menu
+        Debug.Log("Options button clicked");
+        //ui.Q<VisualElement>("OptionsMenu").style.display = DisplayStyle.Flex;
+    }
+    private void OnQuitButtonClicked()
+    {
+        // Quit the application
+        Application.Quit();
+    }
+
+}
