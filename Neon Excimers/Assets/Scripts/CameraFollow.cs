15999f9da80e63cab516c7b7e1e885b8f0694b8e
Stunker192 <dominicarant@gmail.com>
2026-02-13 13:39:07 -0500

    CharacterMovement
    I changed the movement of the player and added a nice camera follow script so it's smooth.

---
+++ Neon Excimers/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    
+    public Transform target;
+    public float smoothTime = 0.3f;
+    public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+    //Start is called before the first from update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            Vector3 targetPosition = target.position + offset;
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
