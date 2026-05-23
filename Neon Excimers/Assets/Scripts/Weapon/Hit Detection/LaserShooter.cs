169a8cb9f5c5ee68b99b4924b51efc38e04099e2
Aiden_with_a_G <agauvin@iu.edu>
2026-05-23 15:29:10 -0400

    Laser Visuals Overhaul
    - Updated the logic to improve the visual effects of laser penetration. Current implementation does not factor in damage fall of from Far Laser or from PenetrationFactor.

--- Neon Excimers/Assets/Scripts/Weapon/Hit Detection/LaserShooter.cs
+++ Neon Excimers/Assets/Scripts/Weapon/Hit Detection/LaserShooter.cs
@@ -67,6 +67,8 @@ public class LaserShooter : MonoBehaviour
     void FireLaser()
     {
 
+        bool Stop_At_Last_Enemy = false;
+
         float Maximum_Full_Laser_Range = (range * 2) * 1.25f;
         Vector3 start = firePoint.position;
 
@@ -91,12 +93,16 @@ public class LaserShooter : MonoBehaviour
         RaycastHit hit;
         Vector3 end;
 
+        // Phase 1: RayCast the laser + far laser
         // Gets all the objects that can be reasonably hit by our laser
         RaycastHit[] All_Objects_Hit;
         All_Objects_Hit = Physics.RaycastAll(start, direction, Maximum_Full_Laser_Range, ~IgnoreThisLayer);
 
-        // This list takes all the objects hit and-
-        // filters for only enemies
+
+
+
+        // Phase 2: Filter
+        // Removes all non-enemy objects
         List<RaycastHit_Comparable> All_Enemies_Hit = new List<RaycastHit_Comparable>();
         for (int i = 0; i < All_Objects_Hit.Length; i++)
         {
@@ -112,29 +118,50 @@ public class LaserShooter : MonoBehaviour
                 All_Enemies_Hit.Add(NewRaycastHit_ToCompare);
             }
         }
-        
 
+
+        // Phase 3: Damage Handling
+        // We have a full list of enemies that can be reasonably hit by the laser
+        // Now we determine damage to these enemies
+
+        
+        // Check if any enemies have been hit
+        int new_I = 0;
         if (All_Enemies_Hit.Count > 0)
         {
-            // orders list from closest to furthest
+            // Phase 3.1: Organization
+            // Organizes the list by distance
+            // Closest --> Furthest
             GameObject HitSound = Instantiate(HitSoundPrefab);
             HitSound.transform.position = All_Enemies_Hit[0].TheRaycastHitItself.transform.position;
             All_Enemies_Hit.Sort();
             
             if (All_Enemies_Hit.Count > 2)
             {
+                
                 for (int i = 0; i < PenetrationFactor; i++)
                 {
-                    RaycastHit Enemy_Hit = All_Enemies_Hit[i].TheRaycastHitItself;
-                    var hp = Enemy_Hit.collider.transform.root.GetComponent<Health_Module>();
-                    if (Enemy_Hit.collider.transform.root.CompareTag("Enemy"))
+                    // Debug.Log(new_I);
+                    new_I = i;
+                    RaycastHit Last_Enemy_Hit = All_Enemies_Hit[i].TheRaycastHitItself;
+                    var hp = Last_Enemy_Hit.collider.transform.root.GetComponent<Health_Module>();
+                    if (Last_Enemy_Hit.collider.transform.root.CompareTag("Enemy"))
                     {
                         if (hp != null)
                             hp.TakeDamage((int)damage);
-                            spawnWeaponEffects(Enemy_Hit.point);
+                            spawnWeaponEffects(Last_Enemy_Hit.point);
                     }
                 }
-                end = All_Enemies_Hit[(PenetrationFactor - 1)].TheRaycastHitItself.point;
+                if ( PenetrationFactor > (new_I + 1) )
+                {
+                    end = start + direction * Maximum_Full_Laser_Range;
+                }
+                else
+                {
+                    Stop_At_Last_Enemy = true;
+                    end = All_Enemies_Hit[(PenetrationFactor - 1)].TheRaycastHitItself.point;
+                }
+                
             } else {
                 RaycastHit Enemy_Hit = All_Enemies_Hit[0].TheRaycastHitItself;
                 var hp = Enemy_Hit.collider.transform.root.GetComponent<Health_Module>();
@@ -144,16 +171,105 @@ public class LaserShooter : MonoBehaviour
                         hp.TakeDamage((int)damage);
                         spawnWeaponEffects(Enemy_Hit.point);
                 }
-                end = All_Enemies_Hit[0].TheRaycastHitItself.point;
+                end = start + direction * Maximum_Full_Laser_Range;
             }
         }
+        // No Enemies hit: simply display the laser at its full range
         else
         {
             end = start + direction * Maximum_Full_Laser_Range;
         };
+
+        
+
+
+        // Phase 4: Visual Reconciliation
+        // Now we display the laser itself, reconciling it with the results of the damage.
+        // The laser should end at the nth (PenetrationFactor) enemy or to its maximum range
+
+        Vector3 Middle = start + direction * (Maximum_Full_Laser_Range / 2);
+        RaycastHit[] Searching_For_Last_Enemy_Hit_NormalLaser;
+        Searching_For_Last_Enemy_Hit_NormalLaser = Physics.RaycastAll(start, direction, range, ~IgnoreThisLayer);
+
+        RaycastHit[] Searching_For_Last_Enemy_Hit_FarLaser;
+        Searching_For_Last_Enemy_Hit_FarLaser = Physics.RaycastAll(start, direction, Maximum_Full_Laser_Range, ~IgnoreThisLayer);
+        
+        // 4.1: Normal laser render
+        if (All_Enemies_Hit.Count > 0 && Stop_At_Last_Enemy == true)
+        {
+            for (int i = 0; i < Searching_For_Last_Enemy_Hit_NormalLaser.Length; i++)
+            {
+                RaycastHit_Comparable Last_Enemy_Hit = All_Enemies_Hit[new_I];
+                RaycastHit newObjHit = Searching_For_Last_Enemy_Hit_NormalLaser[i];
+
+                if (GameObject.ReferenceEquals(newObjHit, Last_Enemy_Hit))
+                {
+                    end = newObjHit.point;
+                    SpawnLaser(start, end);
+                } else
+                {
+                    SpawnLaser(start, Middle);
+                    SpawnFarLaser(Middle, end);
+                }
+        
+            }
+
+            for (int i = 0; i < Searching_For_Last_Enemy_Hit_FarLaser.Length; i++)
+            {
+                RaycastHit_Comparable Last_Enemy_Hit = All_Enemies_Hit[new_I];
+                RaycastHit newObjHit = Searching_For_Last_Enemy_Hit_FarLaser[i];
+
+                if (GameObject.ReferenceEquals(newObjHit, Last_Enemy_Hit))
+                {
+                    end = newObjHit.point;
+                    SpawnLaser(start, Middle);
+                    SpawnFarLaser(Middle, end);
+                } else
+                {
+                    SpawnLaser(start, Middle);
+            SpawnFarLaser(Middle, end);
+                }
+        
+            }
+            
+            /*if (Physics.Raycast(start, direction, out hit, range, ~IgnoreThisLayer))
+            {
+                
+                if (GameObject.ReferenceEquals(hit.collider.gameObject, Last_Enemy_Hit))
+                {
+                    end = hit.point;
+                    SpawnLaser(start, end);
+                }
+            }
+            else
+            {
+                var FarStart = end;
+                var FarEnd = end;
+                if (Physics.Raycast(FarStart, direction, out hit, (range * 1.25f), ~IgnoreThisLayer))
+                {
+                    RaycastHit_Comparable Last_Enemy_Hit = All_Enemies_Hit[new_I];
+                    if (GameObject.ReferenceEquals(hit.collider.gameObject, Last_Enemy_Hit))
+                    {
+                        FarEnd = hit.point;
+                        SpawnLaser(start, Middle);
+                        SpawnFarLaser(Middle ,FarEnd);
+                    } else
+                    {
+                        SpawnLaser(start, Middle);
+                        SpawnFarLaser(Middle, end);
+                    }
+                }
+            }*/
+        } 
+        else
+        {
+            
+            SpawnLaser(start, Middle);
+            SpawnFarLaser(Middle, end);
+        }
+
         
         
-        SpawnLaser(start, end);
 
 
 
